fc5b96e461f91713775eec20ef232dffa2c8fef9
josuemj <98498772+josuemj@users.noreply.github.com>
2025-01-17 23:16:23 -0600

    added a cool loader >:)

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,36 @@ import "./App.css";
 import { getCat } from "./controller";
 function App() {
   const [cat, setCat] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const onClick = async () => {
-      const data = await getCat();
-      setCat(data)
-    };
-
-    return (
-      <>
-        <div>
-          <button onClick={onClick}>get a cat</button>
-
-          {cat ? (
-            <>
-              <img src={cat.image}></img>
-              <p>{cat.fact}</p>
-            </>
-          ) : (
-            <>Click to get a cat</>
-          )}
-        </div>
-      </>
-    );
+  const onClick = async () => {
+    setLoading(true);
+    const data = await getCat();
+    setTimeout(() => {
+      setCat(data);
+      setLoading(false);
+    }, 1000);
   };
 
+  return (
+    <>
+      <div className="cat-container">
+        <button onClick={onClick}>get a cat</button>
+        {loading ? (
+          <>
+            <div className="loader"></div>
+          </>
+        ) : cat ? (
+          <>
+            <h2>{cat.fact}</h2>
+            <img src={cat.image} className="cat-image"></img>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    </>
+  );
+}
 
 export default App;
