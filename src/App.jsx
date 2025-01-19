7443b7a8f7f56ba6edc1781de9c09000983d5f7a
josuemj <98498772+josuemj@users.noreply.github.com>
2025-01-18 22:44:53 -0600

    feat: improved css and UX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getCat } from "./controller";
 function App() {
   const [cat, setCat] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [animate, setAnimate] = useState(false);
 
   const onClick = async () => {
     setLoading(true);
@@ -11,24 +12,39 @@ function App() {
     setTimeout(() => {
       setCat(data);
       setLoading(false);
-    }, 1000);
+      setAnimate(true);
+    }, 1250);
   };
+
+  useEffect(() => {
+    if (cat.fact) {
+      setAnimate(true);
+    }
+  }, [cat]);
 
   return (
     <>
       <div className="cat-container">
-        <button onClick={onClick}>get a cat</button>
+        <button onClick={onClick} className="get-cat-button" style={ loading ? {opacity: 0} : {}}>Get a cat</button>
         {loading ? (
           <>
             <div className="loader"></div>
           </>
         ) : cat ? (
           <>
-            <h2>{cat.fact}</h2>
-            <img src={cat.image} className="cat-image"></img>
+            <h2 className={`cat-text ${animate ? "animating" : ""}`}>
+              {cat.fact}
+            </h2>
+            <img
+              src={cat.image}
+              className={`cat-image ${animate ? "animating" : ""}`}
+              style={cat.image == undefined ? { opacity: 0 } : { opacity: 1 }}
+            ></img>
           </>
         ) : (
-          <></>
+          <>
+            <div>si</div>
+          </>
         )}
       </div>
     </>
